74f6a50554feaa7545f72eff89ec85e3f8958771
DESKTOP-EF0Q5PV\P55 <csj2702@naver.com>
2019-03-19 22:43:03 +0900

    init

---
+++ src/main/java/go/Shop/com/HelloController.java
@@ -0,0 +1,14 @@
+package go.Shop.com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "서버연동  " + new Date() + "\n";
+    }
+}
