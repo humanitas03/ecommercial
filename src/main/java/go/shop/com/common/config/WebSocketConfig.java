8d31d89e2fb61478c93033dd69d821d99920542b
choiseongjun <csj2702@naver.com>
2019-05-09 23:01:34 +0900

    stomp 관련 설정

--- src/main/java/go/shop/com/common/config/WebSocketConfig.java
+++ src/main/java/go/shop/com/common/config/WebSocketConfig.java
@@ -21,6 +21,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.setApplicationDestinationPrefixes("/app");
-        config.enableSimpleBroker("/topic", "/queue");
+        config.enableSimpleBroker("/topic/public", "/queue");
     }
 }
