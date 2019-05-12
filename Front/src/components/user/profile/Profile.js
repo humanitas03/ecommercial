71c6d8ec8d1ad28286b4e82f5246e4f774b9dec2
choiseongjun <csj2702@naver.com>
2019-05-13 00:56:18 +0900

    회원업데이트 추가

--- Front/src/components/user/profile/Profile.js
+++ Front/src/components/user/profile/Profile.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import './Profile.css';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 class Profile extends Component {
     constructor(props) {
         super(props);
         console.log(props);
+        this.state = {
+          name : this.props.currentUser.name,
+          id: this.props.currentUser.id
+        }
+        this.inputFormHandler = this.inputFormHandler.bind(this);
+    }
+    inputFormHandler(e){
+      this.setState({[e.target.name]: e.target.value})
+    }
+    onSubmit(e) {
+        e.preventDefault();
+       
+        axios.post('http://localhost:8080/updateUser')
+        .then(res => console.log(res.data));
+        
+       
     }
     render() {
         return (
+            <form onSubmit={this.onSubmit} className='add-book-form'>
             <div className="profile-container">
                 <div className="container">
                     <div className="profile-info">
                         <div className="profile-avatar">
+                        내사진
                             { 
                                 this.props.currentUser.imageUrl ? (
                                     <img src={this.props.currentUser.imageUrl} alt={this.props.currentUser.name}/>
@@ -23,13 +42,56 @@ class Profile extends Component {
                             }
                         </div>
                         <div className="profile-name">
-                           <h2>{this.props.currentUser.name}</h2>
-                           <p className="profile-email">{this.props.currentUser.email}</p>
+
                            <h2>{this.props.currentUser.roles["0"].name=='Admin'?<NavLink to="adminproductinsert">상품 등록페이지로</NavLink>:''}</h2>
                         </div>
                     </div>
                 </div>    
+                <div className="container h-100">
+  <div className="row h-100">
+    <div className="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+      <div className="d-table-cell align-middle">
+        <div className="text-center mt-4">
+          <h1 className="h2">회원정보 수정</h1>
+          <p className="lead">
+            Start creating the best possible user experience for you customers.
+          </p>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <div className="m-sm-4">
+             
+                <div className="form-group">
+                  <label>Email</label>
+                  <input className="form-control form-control-lg" type="text" 
+                  name="email"  
+                  value={this.props.currentUser.email}
+                  readOnly/>
+                </div>
+                <div className="form-group">
+                  <label>Name</label>
+                  <input className="form-control form-control-lg" 
+                  type="text" 
+                  name="name" 
+                  placeholder="Enter your name" 
+                  value={this.state.name}  
+                  onChange={this.inputFormHandler}/>
+                </div>
+
+               
+                <div className="text-center mt-3">
+                <button>회원정보 수정</button>
+                  {/* <button type="submit" class="btn btn-lg btn-primary">Sign up</button> */}
+                </div>
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</form>
         );
     }
 }
--- src/main/java/go/shop/com/account/controller/UserController.java
+++ src/main/java/go/shop/com/account/controller/UserController.java
@@ -1,8 +1,17 @@
 package go.shop.com.account.controller;
 
+import java.awt.print.Book;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import go.shop.com.account.exception.ResourceNotFoundException;
@@ -10,6 +19,7 @@ import go.shop.com.account.model.User;
 import go.shop.com.account.repository.UserRepository;
 import go.shop.com.account.security.CurrentUser;
 import go.shop.com.account.security.UserPrincipal;
+import go.shop.com.product.domain.Product;
 
 @RestController
 public class UserController {
@@ -23,4 +33,14 @@ public class UserController {
         return userRepository.findById(userPrincipal.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
     }
+    
+    @CrossOrigin
+    @PostMapping("/updateUser")
+    public ResponseEntity<User> updateBook() {
+    	System.out.println("회원업데이트연동");
+    	return null;
+    }
+    
+    
+ 
 }
