ec9cee6923e08a7488487da813bb1eead71d0ad1
choiseongjun <csj2702@naver.com>
2019-05-14 07:32:10 +0900

    회원정보 수정,삭제 완료

--- src/main/java/go/shop/com/account/controller/UserController.java
+++ src/main/java/go/shop/com/account/controller/UserController.java
@@ -1,46 +1,59 @@
 package go.shop.com.account.controller;
 
-import java.awt.print.Book;
-import java.util.List;
+ 
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import go.shop.com.account.exception.ResourceNotFoundException;
 import go.shop.com.account.model.User;
 import go.shop.com.account.repository.UserRepository;
 import go.shop.com.account.security.CurrentUser;
 import go.shop.com.account.security.UserPrincipal;
-import go.shop.com.product.domain.Product;
+
+ 
 
 @RestController
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @GetMapping("/user/me")
-    @PreAuthorize("hasRole('USER')")
-    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
-        return userRepository.findById(userPrincipal.getId())
-                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
-    }
-    
-    @CrossOrigin
-    @PostMapping("/updateUser")
-    public ResponseEntity<User> updateBook() {
-    	System.out.println("회원업데이트연동");
-    	return null;
-    }
-    
-    
- 
+	@GetMapping("/user/me")
+	@PreAuthorize("hasRole('USER')")
+	public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+		return userRepository.findById(userPrincipal.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+	}
+	@PostMapping("/updateUser/{user.id}")
+	public ResponseEntity<User> updateUser(@PathVariable("user.id") Long id, @RequestBody User user) {
+		System.out.println("Update User" + user);
+		Optional<User> userData = userRepository.findById(id);
+		if (userData.isPresent()) {
+			User saveUser = userData.get();
+			saveUser.setName(user.getName());
+			User updatedBook = userRepository.save(saveUser);
+			return new ResponseEntity<>(updatedBook, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	@DeleteMapping("/deleteUser/{user.id}")
+	  public ResponseEntity<String> deleteUser(@PathVariable("user.id") Long id) {
+	    System.out.println("Delete Book with ID = " + id + "...");
+	    try {
+	    	userRepository.deleteById(id);
+	    } catch (Exception e) {
+	      return new ResponseEntity<>("Fail to delete!", HttpStatus.EXPECTATION_FAILED);
+	    }
+	    return new ResponseEntity<>("Book has been deleted!", HttpStatus.OK);
+	  }
 }
